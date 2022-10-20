2a3aeb05c1534150a2c0e5f0388f051a0f0f3851
Fedor <example@gmail.com>
2022-10-20 19:50:42 +0300

    version_02

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let money = prompt("Ваш бюджет на месяц?")
 let time = prompt("Введите дату рождения")
 
-let question_1 = prompt("Введите статью расходов")
-let question_2 = prompt("Во сколько обойдется")
+let a1 = prompt("Введите статью расходов")
+let a2 = prompt("Во сколько обойдется")
+let a3 = prompt("Введите статью расходов")
+let a4 = prompt("Во сколько обойдется")
+
 
 let appData = {
 moneys: money,
@@ -12,5 +15,8 @@ optionalExpenses: {},
 income: [],
 savings: false
 }
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
 alert(money / 30)
