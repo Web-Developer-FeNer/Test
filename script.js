91af15266bb66fa2e322a9a37f2037b99328ec53
Fedor <example@gmail.com>
2022-10-28 19:14:00 +0300

    fronted

--- script.js
+++ script.js
@@ -18,10 +18,8 @@ timeData: time,
 expenses: {},
 optionalExpenses: {},
 income: [],
-savings: false
-}
-
-function chooseExpenses() {
+savings: false, 
+chooseExpenses: function(){
     for (let i=0; i<2; i++){
         let a = prompt("Введите статью расходов")
         let b = prompt("Во сколько обойдется")
@@ -33,14 +31,39 @@ function chooseExpenses() {
             i = i - 1
         }
     }
+},
+detectDayBudget: function(){
+    appData.moneyPerDay = (appData.moneys / 30).tiFixed()
+    alert("Ежедневный бюджет " + moneyPerDay)
+},
+detectLevel: function() {
+    if (appData.moneyPerDay < 100){
+        console.log("Минимальный бюджет")
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+        console.log("Средний бюджет")
+    } else if (appData.moneyPerDay > 2000){
+        console.log("Высокий уровень бюджет")
+    } else {
+        console.log("Ошибка")
+    }
+},
+checkSavings: function() {
+    if (appData.saving == true){
+        let save = prompt("Какова сумма накоплений"),
+            percent = prompt("Под какой процент")
+
+        appData.monthIncome = save/100/12*percent
+        alert("Доход в месяц - " + appData.monthIncome)
+    }
+},
+chooseOptExpenses: function(){
+    for(let i=0; i<3; i++){
+        let opt = prompt("Статья необязтаельных расходов")
+        appData.optionalExpenses[i] = opt
+    }
+},
+chooseIncome: function(){
+    let items = prompt("Что принесет дополнительный доход ? (перечислите через запятую)")
+    appData.income = items.split(', ')
 }
-chooseExpenses()
-
-
-
-
-appData.moneyDay = (appData.moneys / 30).toFixed()
-
-
-
-alert(appData.moneyDay)
+}
